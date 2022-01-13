dca8715098819ebd88951080e9ee703a0b94a966
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-01-12 19:14:41 -0700

    local weather

--- index.js
+++ index.js
@@ -2,33 +2,57 @@ import React from "react";
 import axios from "axios";
 
 export default class Local extends React.Component {
-
   state = {
     lat: "",
-    lon: ""
+    lon: "",
+    localWeather: null,
+    weatherIcon: "",
+    isLoading: false,
+    hasError: false,
+    userMessage: ""
   };
 
   getLocalWeather = async () => {
+    this.setState({ isLoading: true });
     try {
-      const response = await axios(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&appid=e796806e429831a5c613d003290850bf
+      const {
+        data
+      } = await axios(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=imperial&appid=e796806e429831a5c613d003290850bf
       `);
-      console.log(response.data);
+      const iconId = data.weather[0].icon;
+      const weatherIcon = "http://openweathermap.org/img/w/" + iconId + ".png";
+      this.setState({
+        localWeather: data,
+        weatherIcon,
+        isLoading: false,
+        userMessage: "",
+        hasError: false
+      });
     } catch (err) {
+      this.setState({
+        isLoading: false,
+        hasError: true,
+        userMessage: "Local weather not available"
+      });
       console.log(err);
     }
   };
 
   getLocation = () => {
+    this.setState({ isLoading: true });
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
+      navigator.geolocation.getCurrentPosition((position) => {
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
-        this.setState({ lat, lon });
-        console.log(lat, lon);
+        this.setState({ lat, lon, isLoading: false });
+        this.getLocalWeather();
       });
-      this.getLocalWeather();
     } else {
-      console.log("Location Not Available");
+      this.setState({
+        isLoading: false,
+        hasError: true,
+        userMessage: "User Location Not Available"
+      });
     }
   };
 
@@ -41,6 +65,39 @@ export default class Local extends React.Component {
       <div>
         <h1>Weather App</h1>
         <h2>Current weather for your location:</h2>
+        {this.state.hasError && <div>{this.state.userMessage}</div>}
+        {this.state.isLoading && <div>Loading local weather...</div>}
+
+        {this.state.localWeather && (
+          <>
+            <div>
+              {this.state.localWeather.name},{" "}
+              {this.state.localWeather.sys.country}
+            </div>
+            <img src={this.state.weatherIcon} alt="weather icon" />
+            <div>
+              Current temperature:{" "}
+              {this.state.localWeather.main.temp.toFixed(0)}° F
+            </div>
+            <div>
+              Feels like: {this.state.localWeather.main.feels_like.toFixed(0)}°
+              F
+            </div>
+            <div>
+              Description: {this.state.localWeather.weather[0].main}(
+              {this.state.localWeather.weather[0].description})
+            </div>
+            <div>
+              Today's low: {this.state.localWeather.main.temp_min.toFixed(0)}° F
+            </div>
+            <div>
+              Today's high: {this.state.localWeather.main.temp_max.toFixed(0)}°
+              F
+            </div>
+            <div>Humidity: {this.state.localWeather.main.humidity}%</div>
+            <div>Wind: {this.state.localWeather.wind.speed.toFixed(0)} mph</div>
+          </>
+        )}
       </div>
     );
   }
